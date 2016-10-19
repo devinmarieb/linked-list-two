ee2e925a9a20f15096d446fb697b9d1497b923fe
Devin Beliveau <devinmarieb@gmail.com>
2016-10-19 12:58:11 -0600

    Add bookmark counter

--- scripts.js
+++ scripts.js
@@ -1,19 +1,35 @@
 var bookmarkField = $('.bookmark');
 var urlField = $('.url');
+var counter = $('li').length;
+var readCounter = $('.read').length;
+
+function countBookmarks(){
+  counter++;
+  $('.total-bookmarks').text('Total Bookmarks: ' + counter);
+}
+
+function countReadBookmarks(){
+  $('.read-bookmarks').text('Read Bookmarks: ' + readCounter.val());
+}
 
 
 function addCard(bookmark, url){
-  var bookmarkEntry = `<p class="bookmark-name">${bookmark}</p>`;
-  var urlEntry = `<a class="url-name" href=${url}>${url}</a>`;
-  var readButton = `<button class="read-button" type="button" name="read">Read</button>`;
-  var deleteButton = `<button class="delete-button" type="button" name="delete">Delete</button>`;
-  var newCard = $('.card-section').append(`<li class="new-card">${bookmarkEntry}<hr class="first-hr">${urlEntry}<hr class="second-hr"><section class="button-section">${readButton}${deleteButton}</section></li>`);
-}
+  var newCard = $('.card-section').append(`<li class="new-card">
+                                            <p class="bookmark-name">${bookmark}</p>
+                                            <hr class="first-hr">
+                                            <a class="url-name" href=${url}>${url}</a>
+                                            <hr class="second-hr">
+                                            <section class="button-section">
+                                              <button class="read-button" type="button" name="read">Read</button>
+                                              <button class="delete-button" type="button" name="delete">Delete</button>
+                                            </section>
+                                          </li>`);
+                              }
 
 function checkFields(bookmark, url){
   if(bookmark === '' || url === ''){
     $('.submit-button').disabled = true;
-    $('.error').text('¡ Please enter a bookmark and URL !');
+    $('.error').text('¡ Please enter a title and URL !');
   } else {
     addCard(bookmark, url);
   }
@@ -29,13 +45,17 @@ $('.submit-button').on('click', function(){
   var url = urlField.val();
   checkFields(bookmark, url);
   clearFields(bookmark, url);
+  countBookmarks();
 })
 
 $('ul').on('click', '.read-button', function(){
   $(this).closest('li').toggleClass('read');
   $(this).closest('button').toggleClass('read');
+  countReadBookmarks();
 })
 
 $('ul').on('click', '.delete-button', function(){
   $(this).closest('li').remove();
+  counter--;
+  $('.total-bookmarks').text('Total Bookmarks: ' + counter);
 })
