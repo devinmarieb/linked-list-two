2ac52604ab1923b649ae1aaeb4f1e3c15dfd4b48
Devin Beliveau <devinmarieb@gmail.com>
2016-10-18 20:47:53 -0600

    Get delete/read class functionality

--- scripts.js
+++ scripts.js
@@ -3,15 +3,34 @@ var urlField = $('.url');
 
 
 function addCard(bookmark, url){
-  var newCard = $('.card-section').append(`<li class="new-card">${bookmark}${url}</li>`);
-  
-
+  var bookmarkEntry = `<p class="bookmark-name">${bookmark}</p>`;
+  var urlEntry = `<a class="url-name" href=${url}>${url}</a>`;
+  var readButton = `<button class="read-button" type="button" name="read">Read</button>`;
+  var deleteButton = `<button class="delete-button" type="button" name="delete">Delete</button>`;
+  var newCard = $('.card-section').append(`<li class="new-card">${bookmarkEntry}${urlEntry}${readButton}${deleteButton}</li>`);
 }
 
+function checkFields(bookmark, url){
+  if(bookmark === '' || url === ''){
+    $('.submit-button').disabled = true;
+    $('.error').text('Please enter a bookmark and url!');
+  } else {
+    addCard(bookmark, url);
+  }
+}
 
 $('.submit-button').on('click', function(){
-  debugger
   var bookmark = bookmarkField.val();
   var url = urlField.val();
-  addCard(bookmark, url);
-});
+  checkFields(bookmark, url);
+  var bookmark = bookmarkField.val('');
+  var url = urlField.val('');
+})
+
+$('ul').on('click', '.read-button', function(){
+  $(this).closest('li').toggleClass('read');
+})
+
+$('ul').on('click', '.delete-button', function(){
+  $(this).closest('li').remove();
+})
